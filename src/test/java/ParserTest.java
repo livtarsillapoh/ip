81525e55a00c77eec141b2a4e3b787d8f88d25cc
tara <e1366553@u.nus.edu>
2025-09-01 16:28:23 +0800

    A-JUnit

---
+++ src/test/java/ParserTest.java
@@ -0,0 +1,24 @@
+import tarawrr.*;  //same package as the class being tested
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+
+public class ParserTest {
+    @Test
+    public void parseValidInput() throws Exception {
+        String input = "todo do homework";
+        Command command = Parser.parseTask(input);
+        assertInstanceOf(TodoCommand.class, command);
+    }
+
+    @Test
+    public void parseInvalidInput() throws Exception {
+        String input = "blah";
+        try {
+            Parser.parseTask(input);
+        } catch (TarawrrException e) {
+            assertEquals("OOPS!!! I'm sorry, but I don't know what that means :-(",
+                    e.getMessage());
+        }
+    }
+}
